100b982be3aa95368da5f7cacf0ca45a89444552
Adam Riggs <adam@adamriggs.com>
2014-08-29 22:48:44 -0400

    first commit

---
+++ Interestingr/Interestingr/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Interestingr
+//
+//  Created by Adam Riggs on 8/29/14.
+//  Copyright (c) 2014 Adam Riggs. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ readme.md
@@ -0,0 +1,3 @@
+Interestingr
+
+This is something that I write when I'm learning a new language.  It gets the list of the most interesting photos on Flickr.com and displays them for the user.
