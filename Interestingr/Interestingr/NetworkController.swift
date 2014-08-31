1da456d614ce3c2cbbaaf59a5a299331655c33c2
Adam Riggs <adam@adamriggs.com>
2014-08-31 13:47:49 -0400

    gets data from flickr, reads the xml, outputs a html file

--- Interestingr/Interestingr/NetworkController.swift
+++ Interestingr/Interestingr/NetworkController.swift
@@ -8,59 +8,69 @@
 
 import Cocoa
 
-class NetworkController: NSObject {
+class NetworkController: NSObject, NSXMLParserDelegate {
     
-//    func searchItunesFor(searchTerm: String) {
-//        
-//        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
-//        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-//        
-//        // Now escape anything else that isn't URL-friendly
-//        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
-//            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
-//            let url: NSURL = NSURL(string: urlPath)
-//            let session = NSURLSession.sharedSession()
-//            let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
-//                println("Task completed")
-//                if(error != nil) {
-//                    // If there is an error in the web request, print it to the console
-//                    println(error.localizedDescription)
-//                }
-//                var err: NSError?
-//                
-//                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
-//                if(err != nil) {
-//                    // If there is an error parsing JSON, print it to the console
-//                    println("JSON Error \(err!.localizedDescription)")
-//                }
-//                let results: NSArray = jsonResult["results"] as NSArray
-//                dispatch_async(dispatch_get_main_queue(), {
-//                    self.tableData = results
-//                    self.appsTableView!.reloadData()
-//                    })
-//                })
-//            
-//            task.resume()
-//        }
-//    }
+    var flickrOutput = ""
     
     func getNetReq(reqString:String){
         let mySession = NSURLSession.sharedSession()
         let url:NSURL = NSURL(string:reqString)
-        println("1")
+        
         let networkTask = mySession.dataTaskWithURL(url, completionHandler : {data, response, error -> Void in
             var err:NSError?
 //            var theJSON = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSMutableDictionary
 //            let results : NSArray = theJSON["data"]!["children"] as NSArray
+            //var xml = NSXMLParser(data: data)
             dispatch_async(dispatch_get_main_queue(), {
                 println(data)
-                println("\n")
-                println(response)
+                //println("\n")
+                //println(response)
                 })
             })
-        println("2")
         networkTask.resume()
-        println("3")
+    }
+    
+    func getXML(reqString:String){
+        //var parser = NSXMLParser()
+        var url: NSURL = NSURL.URLWithString(reqString)
+        
+        var parser = NSXMLParser (contentsOfURL: url)
+        
+        parser.delegate = self
+        
+        parser.shouldProcessNamespaces = false
+        
+        parser.shouldReportNamespacePrefixes = false
+        
+        parser.shouldResolveExternalEntities = false
+        
+        parser.parse()
+    }
+    
+    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: NSDictionary!) {
+        //println("Element's name is \(elementName)")
+        //println("Element's attributes are \(attributeDict)")
+        
+        //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+        
+        var file = "/Users/adam/Documents/Experiments/swift/osx/interestingr-osx/interestingness.html"
+        
+        if(elementName == "photo"){
+            //println(attributeDict["farm"])
+            flickrOutput += "<img src ='https://farm"
+            flickrOutput += attributeDict["farm"] as NSString
+            flickrOutput += ".staticflickr.com/"
+            flickrOutput += attributeDict["server"] as NSString
+            flickrOutput += "/"
+            flickrOutput += attributeDict["id"] as NSString
+            flickrOutput += "_"
+            flickrOutput += attributeDict["secret"] as NSString
+            flickrOutput += ".jpg' />\n"
+        }
+        
+        //println(flickrOutput)
+        flickrOutput.writeToFile(file, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
+        
     }
 
 }
