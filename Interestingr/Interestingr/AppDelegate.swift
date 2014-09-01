602f45c7ada60549589e28866cc9ed955929da0f
Adam Riggs <adam@adamriggs.com>
2014-09-01 00:50:05 -0400

    succefully loaded a local image and displayed it in the main window

--- Interestingr/Interestingr/AppDelegate.swift
+++ Interestingr/Interestingr/AppDelegate.swift
@@ -11,13 +11,17 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
     @IBOutlet var window: NSWindow
-
-
+    @IBOutlet var mainImage: NSImageView
+    
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
-        var net = NetworkController()
+        var flickr = NetworkController()
         
-        net.getXML("https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=2b6e6ffa4d1850dad7ee4973d77d6edf")
+        flickr.getXML("https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=2b6e6ffa4d1850dad7ee4973d77d6edf")
+        
+        var url:NSURL = NSURL.URLWithString("https://farm6.staticflickr.com/5557/15057740736_00fc1f196a.jpg")
+//        var data:NSData = NSData.dataWithContentsOfURL(url, options: nil, error: nil)
+        mainImage.image = NSImage(byReferencingURL: url)
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
