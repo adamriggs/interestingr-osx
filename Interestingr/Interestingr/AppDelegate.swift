5405f0fa6c42fe83c91ed114da5051d2c254e9e1
Adam Riggs <adam@adamriggs.com>
2014-09-03 00:44:19 -0400

    the image in the app is now based on the flickr data from the network request

--- Interestingr/Interestingr/AppDelegate.swift
+++ Interestingr/Interestingr/AppDelegate.swift
@@ -13,21 +13,30 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet var window: NSWindow
     @IBOutlet var mainImage: NSImageView
     
+    var interestingnessArray: [String] = []
+    
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
         var flickr = NetworkController()
+        flickr.parent = self
         
         flickr.getXML("https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=2b6e6ffa4d1850dad7ee4973d77d6edf")
         
-        var url:NSURL = NSURL.URLWithString("https://farm6.staticflickr.com/5557/15057740736_00fc1f196a.jpg")
+        var url:NSURL = NSURL.URLWithString("https://farm4.staticflickr.com/3858/14917248907_091bcded99.jpg")
 //        var data:NSData = NSData.dataWithContentsOfURL(url, options: nil, error: nil)
-        mainImage.image = NSImage(byReferencingURL: url)
+        //mainImage.image = NSImage(byReferencingURL: url)
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
-
+    
+    func flickrLoaded(urlString: String){
+        println("flickrLoaded()")
+        interestingnessArray.append(urlString)
+        var url:NSURL = NSURL.URLWithString(urlString)
+        mainImage.image = NSImage(byReferencingURL: url)
+    }
 
 }
 
--- Interestingr/Interestingr/NetworkController.swift
+++ Interestingr/Interestingr/NetworkController.swift
@@ -11,37 +11,35 @@ import Cocoa
 class NetworkController: NSObject, NSXMLParserDelegate {
     
     var flickrOutput = ""
+    var parent: AppDelegate = AppDelegate()
     
-    func getNetReq(reqString:String){
-        let mySession = NSURLSession.sharedSession()
-        let url:NSURL = NSURL(string:reqString)
-        
-        let networkTask = mySession.dataTaskWithURL(url, completionHandler : {data, response, error -> Void in
-            var err:NSError?
-//            var theJSON = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSMutableDictionary
-//            let results : NSArray = theJSON["data"]!["children"] as NSArray
-            //var xml = NSXMLParser(data: data)
-            dispatch_async(dispatch_get_main_queue(), {
-                println(data)
-                //println("\n")
-                //println(response)
-                })
-            })
-        networkTask.resume()
-    }
+//    func getNetReq(reqString:String){
+//        let mySession = NSURLSession.sharedSession()
+//        let url:NSURL = NSURL(string:reqString)
+//        
+//        let networkTask = mySession.dataTaskWithURL(url, completionHandler : {data, response, error -> Void in
+//            var err:NSError?
+////            var theJSON = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSMutableDictionary
+////            let results : NSArray = theJSON["data"]!["children"] as NSArray
+//            //var xml = NSXMLParser(data: data)
+//            dispatch_async(dispatch_get_main_queue(), {
+//                println(data)
+//                //println("\n")
+//                //println(response)
+//                })
+//            })
+//        networkTask.resume()
+//    }
     
     func getXML(reqString:String){
         //var parser = NSXMLParser()
-        var url: NSURL = NSURL.URLWithString(reqString)
         
+        var url: NSURL = NSURL.URLWithString(reqString)
         var parser = NSXMLParser (contentsOfURL: url)
         
         parser.delegate = self
-        
         parser.shouldProcessNamespaces = false
-        
         parser.shouldReportNamespacePrefixes = false
-        
         parser.shouldResolveExternalEntities = false
         
         parser.parse()
@@ -54,23 +52,41 @@ class NetworkController: NSObject, NSXMLParserDelegate {
         //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
         
         var file = "/Users/adam/Documents/Experiments/swift/osx/interestingr-osx/interestingness.html"
+        var url = ""
         
         if(elementName == "photo"){
-            //println(attributeDict["farm"])
-            flickrOutput += "<img src ='https://farm"
-            flickrOutput += attributeDict["farm"] as NSString
-            flickrOutput += ".staticflickr.com/"
-            flickrOutput += attributeDict["server"] as NSString
-            flickrOutput += "/"
-            flickrOutput += attributeDict["id"] as NSString
-            flickrOutput += "_"
-            flickrOutput += attributeDict["secret"] as NSString
-            flickrOutput += ".jpg' />\n"
+            url += "https://farm"
+            url += attributeDict["farm"] as NSString
+            url += ".staticflickr.com/"
+            url += attributeDict["server"] as NSString
+            url += "/"
+            url += attributeDict["id"] as NSString
+            url += "_"
+            url += attributeDict["secret"] as NSString
+            url += ".jpg"
         }
+        
+        flickrOutput += "<img src ='"
+        flickrOutput += url
+        flickrOutput += "' />\n"
+        
+//        if(elementName == "photo"){
+//            //println(attributeDict["farm"])
+//            flickrOutput += "<img src ='https://farm"
+//            flickrOutput += attributeDict["farm"] as NSString
+//            flickrOutput += ".staticflickr.com/"
+//            flickrOutput += attributeDict["server"] as NSString
+//            flickrOutput += "/"
+//            flickrOutput += attributeDict["id"] as NSString
+//            flickrOutput += "_"
+//            flickrOutput += attributeDict["secret"] as NSString
+//            flickrOutput += ".jpg' />\n"
+//        }
         
         //println(flickrOutput)
         flickrOutput.writeToFile(file, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
         
+        parent.flickrLoaded(url)
     }
 
 }
